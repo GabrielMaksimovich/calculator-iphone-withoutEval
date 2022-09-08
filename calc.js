ac0536c479a290700281443b1fbb099f02a49df2
GabrielMaksimovich <sdelanvdnb@gmail.com>
2022-09-08 16:26:14 +0300

    Add functions and clear ending dots

--- calc.js
+++ calc.js
@@ -1,99 +1,97 @@
-let a = ''; //first number
-let b = ''; //second number
-let sign = ''; //sign of operation
-let finish = false;
+let a = '' //first number
+let b = '' //second number
+let sign = '' //sign of operation
+let finish = false
 
-const digit = ['0','1','2','3','4','5','6','7','8','9','.'];
-const action = ['-','+','x','/','%'];
+const digit = ['0','1','2','3','4','5','6','7','8','9','.']
+const action = ['-','+','x','/','%']
 
 // desktop
-let out = document.querySelector('.calc-screen p');
+let out = document.querySelector('.calc-screen p')
 
 function clearAll() {
-     a = ''; //first number
-     b = ''; //second number
-     sign = ''; //sign of operation
-     finish = false;
-    setScreenInputValue(0);
+     a = '' //first number
+     b = '' //second number
+     sign = '' //sign of operation
+     finish = false
+        setScreenInputValue(0)
 }
 // -- CODE STYLE
 // 1. Segregate code into functions
 // 2. Use constant for empty value or function
 function hasEmptyValue(val) {
-    return val === '';
+    return val === ''
 }
 function setScreenInputValue(value) {
-   out.textContent = value;
+   out.textContent = value
 }
 function isDigit(key) {
-    return digit.includes(key);
+    return digit.includes(key)
 }
 function calcPercent(number,percent) {
-    return number * percent / 100;
+    return number * percent / 100
 }
 
 document.querySelector('.buttons').onclick = (event) => {
         if(!event.target.classList.contains('btn')) return
+        if(event.target.classList.contains('ac')) return
 
-        if(event.target.classList.contains('ac')) return;
+       setScreenInputValue('')
 
-       setScreenInputValue('');
-
-        const key = event.target.textContent;
+        const key = event.target.textContent
         //if click from 0-9
         if(isDigit(key)) {
             if (hasEmptyValue(b) && hasEmptyValue(sign)) {
-                a += key;
+                a += key
                 if (a[0] === '0' && (a[1] && a[1] !== '.')) {
                     // out.textContent = a
                     a =  parseInt(a)
-                    out.textContent  = a
+                    setScreenInputValue(a)
                 } else {
-                    setScreenInputValue(a);
+                    setScreenInputValue(a)
                 }
 
                 console.log(a)
             } else if ( a!== '' && b!== '' && finish) {
-                b = key;
-                finish = false;
-                out.textContent = b;
+                b = key
+                finish = false
+                setScreenInputValue(b)
             }
             else {
-                b += key;
-                out.textContent = b;
+                b += key
+                setScreenInputValue(b)
             }
-            console.log(a,b,sign)
             return;
         }
 
         //if click on digit -,+,*,/
         if(action.includes(key)) {
-            sign = key;
-            console.log( sign)
-            out.textContent = sign;
+            sign = key
+            setScreenInputValue(sign)
         }
+
         if( key === '%') {
-            if( b === '') {
+            if(hasEmptyValue(b)) {
                 a = calcPercent(a,1)
-                out.textContent = a;
+                setScreenInputValue(a)
                 sign = ""
             } else {
                 if( b !== '' && a !== ''  ) {
                     a = calcPercent(a,b)
-                    out.textContent = a;
+                    setScreenInputValue(a)
                 }
             }
 
         }
-        if(key === '+/-') {
 
-            console.log(a, key)
+        // for +/- key pressed
+        if(key === '+/-') {
             if ( a !== '') {
                 b = -b
-                out.textContent = b;
+                setScreenInputValue(b)
             } else {
                 a = -a
-                out.textContent = a;
+                setScreenInputValue(a)
             }
         }
 
@@ -112,21 +110,16 @@ document.querySelector('.buttons').onclick = (event) => {
                     break;
                 case "/":
                     if ( b == '0') {
-                        out.textContent = 'Error';
-                        a = '';
-                        b = '';
-                        sign = '';
+                        setScreenInputValue('Error')
+                        a = ''
+                        b = ''
+                        sign = ''
                         return
                     }
-                    a = a / b;
+                    a = a / b
                     break;
             }
             finish = true
-            out.textContent = a;
-            console.log(a,b,sign)
+            setScreenInputValue(a)
         }
-
-        // click equal
-
-
 }
